f6dda1046800ae5e74d1f38982167783f9a7643d
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-04 23:09:10 +0700

    main.tf: add comment to terraform

--- main.tf
+++ main.tf
@@ -98,12 +98,12 @@ resource "aws_dynamodb_table" "terraform_locks" {
 }
 
 # enable terraform remote backend and state locking
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-1234567890"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
-  }
-}
+# terraform {
+#   backend "s3" {
+#     bucket         = "terraform-state-bucket-1234567890"
+#     key            = "terraform.tfstate"
+#     region         = "us-east-1"
+#     dynamodb_table = "terraform_locks"
+#   }
+# }
 

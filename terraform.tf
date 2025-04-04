3d3211f6b010f022777a8bcbc6758a333dcf3066
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-04 16:23:09 +0700

    terraform.tf: removed file

--- terraform.tf
+++
@@ -1,9 +0,0 @@
-# enable terraform remote backend and state locking
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-1234567890"
-    key            = "terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform_locks"
-  }
-}

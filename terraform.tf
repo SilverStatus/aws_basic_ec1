bdbcd8c098e2ca46f50db39bb26673814a8a3e0d
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-04 16:18:49 +0700

    terraform.tf: edit bucket & dynamodb_table

--- terraform.tf
+++ terraform.tf
@@ -1,9 +1,9 @@
 # enable terraform remote backend and state locking
 terraform {
   backend "s3" {
-    bucket         = aws_s3_bucket.terraform_state.bucket
+    bucket         = "terraform-state-bucket-1234567890"
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = aws_dynamodb_table.terraform_locks.name
+    dynamodb_table = "terraform_locks"
   }
 }

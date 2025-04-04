8ffd5c4888b6d8c034ce2ce8cecf5573def970d7
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-05 05:27:13 +0700

    main.tf: comment s3 & dynamodb block

--- main.tf
+++ main.tf
@@ -69,39 +69,39 @@ output "public_ip" {
 }
 
 # create s3 for terraform state
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-bucket-101003"
-  force_destroy = "true" # Allow force destroy for testing purposes  
+# resource "aws_s3_bucket" "terraform_state" {
+#   bucket = "terraform-state-bucket-101003"
+#   force_destroy = "true" # Allow force destroy for testing purposes  
 
-  tags = {
-    Name        = "terraform-state-bucket"
-    Environment = "Terraform"
-  }
+#   tags = {
+#     Name        = "terraform-state-bucket"
+#     Environment = "Terraform"
+#   }
   
-}
+# }
 
-# create dynamodb table for state locking
-resource "aws_dynamodb_table" "terraform_locks" {
-  name         = "terraform_locks"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
+# # create dynamodb table for state locking
+# resource "aws_dynamodb_table" "terraform_locks" {
+#   name         = "terraform_locks"
+#   billing_mode = "PAY_PER_REQUEST"
+#   hash_key     = "LockID"
 
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
+#   attribute {
+#     name = "LockID"
+#     type = "S"
+#   }
 
-  tags = {
-    Name        = "terraform_locks"
-    Environment = "Terraform"
-  }
+#   tags = {
+#     Name        = "terraform_locks"
+#     Environment = "Terraform"
+#   }
   
-}
+# }
 
 # enable terraform remote backend and state locking
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-bucket-101003"
+    bucket         = "terraform-jenkins-101001"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"

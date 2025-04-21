f730e6692594a693ee06849b08fce6d852997af4
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-21 23:29:08 +0700

    main.tf: edit ingress rule for ec2

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_security_group" "allow_ssh_and_http" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["118.99.115.149/0"]  # Allow HTTP traffic from anywhere
+    cidr_blocks = ["118.99.115.149/32"]  # Allow HTTP traffic from anywhere
   }
 
   # Outbound rule (allow all traffic)

75db72862e49d507c20b84fbbc5334357165529f
Ahmad Thoriq Azzam <azzam@Ahmads-MacBook-Pro.local>
2025-04-21 22:35:31 +0700

    main.tf: enable remote state and edit ec2 config

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ output "public_ip" {
 # enable terraform remote backend and state locking
 terraform {
   backend "s3" {
-    bucket         = "terraform-jenkins-101001"
+    bucket         = "terraform-101001"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform_locks"
